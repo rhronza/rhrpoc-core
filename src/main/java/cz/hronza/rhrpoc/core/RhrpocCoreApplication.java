3b608761fb7ec487d38a0956f51b0dc79ff003fe
Roman Hronza <roman.hronza@gmail.com>
2022-11-14 17:01:55 +0100

    create spring boot project

---
+++ src/main/java/cz/hronza/rhrpoc/core/RhrpocCoreApplication.java
@@ -0,0 +1,13 @@
+package cz.hronza.rhrpoc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhrpocCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhrpocCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/hronza/rhrpoc/core/RhrpocCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.hronza.rhrpoc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhrpocCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
