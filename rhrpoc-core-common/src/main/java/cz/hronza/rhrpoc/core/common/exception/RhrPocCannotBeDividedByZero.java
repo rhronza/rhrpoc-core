19f26a451e4db7157700f7aa776860a7b415bb6f
Roman Hronza <roman.hronza@gmail.com>
2023-01-09 18:41:15 +0100

    update RhrPocCannotBeDividedByZero exception

--- rhrpoc-core-common/src/main/java/cz/hronza/rhrpoc/core/common/exception/RhrPocCannotBeDividedByZero.java
+++ rhrpoc-core-common/src/main/java/cz/hronza/rhrpoc/core/common/exception/RhrPocCannotBeDividedByZero.java
@@ -6,10 +6,10 @@ import java.util.List;
 /**
  * exception cannot by divided by zero
  */
-public class RhrCannotBeDividedByZero extends RuntimeException {
-    final private List<String> params;
+public class RhrPocCannotBeDividedByZero extends RuntimeException {
+    private final  List<String> params;
 
-    public RhrCannotBeDividedByZero(String emsg, String... params) {
+    public RhrPocCannotBeDividedByZero(String emsg, String... params) {
         super(emsg);
         this.params = new ArrayList<>(List.of(params));
     }
